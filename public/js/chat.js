a996c76e57d9acaa900344f990db6101513c5241
Diego de Biagi <diegobiagiviana@gmail.com>
2016-07-18 19:14:54 -0300

    First commit.

---
+++ public/js/chat.js
@@ -0,0 +1,14 @@
+
+
+function Chat(e) {
+    
+    var __constructor = function (e) {
+        if (!(e instanceof Element)) {
+            throw 'The e parameter needs to be a DOM Element.'
+        } else if (e.tagName !== 'DIV') {
+            throw 'The element needs to be a DIV'
+        }
+    }(e)
+    
+    
+}
