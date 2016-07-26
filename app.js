50bbee62f384b33f713d598fea29d056f48af78e
Diego de Biagi <diegobiagiviana@gmail.com>
2016-07-25 23:47:17 -0300

    Using jade to templates. Unfinished socket module.

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var express = require('express'),
     config = require(__dirname + '/app/config.json'),
     cookieSession = require('cookie-session'),
     session = require('express-session'),
-    bodyParser = require('body-parser')
+    bodyParser = require('body-parser'),
+    socket = require(__dirname + '/app/modules/socket.js')
 
 app.use(session({
     secret: 'keyboard cat',
@@ -13,6 +14,7 @@ app.use(session({
     cookie: { secure: false },
     maxAge: 64000
 }))
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(__dirname + '/app/public'))
@@ -23,6 +25,8 @@ app.use(require(__dirname + '/app/controllers'))
 app.set('views', __dirname + '/app/views');
 app.set('view engine', 'jade');
 app.set('port', config.port)
+
+socket.initialize(http)
 
 http.listen(app.get('port'), function () {
     console.log('listening on *:' + app.get('port'))
